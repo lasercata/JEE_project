88c79fad2db57d7eed9e32d2ba1f29a14097572e
Laszlo <labadie@enssat.fr>
2025-05-02 15:19:32 +0200

    Character and Show class added, HourRange class getDuration method, Hour class toMinutes method, EventTime class

---
+++ code/src/main/java/time/EventTime.java
@@ -0,0 +1,32 @@
+package time;
+
+public class EventTime {
+    private String day;
+    private HourRange time;
+
+    public EventTime(String day, HourRange time) {
+        this.day = day;
+        this.time = time;
+    }
+
+    public String getDay() {
+        return this.day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public HourRange getTime() {
+        return this.time;
+    }
+
+    public void setTime(HourRange time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "le " + this.day + " de " + this.time.getOpening() + " à " + this.time.getEnding();
+    }
+}
---
+++ code/src/main/java/time/Hour.java
@@ -0,0 +1,38 @@
+package time;
+
+public class Hour {
+    private int h;
+    private int m;
+
+    public Hour(int h, int m) {
+        this.h = h;
+        this.m = m;
+    }
+
+    public int getH() {
+        return this.h;
+    }
+
+    public int getM() {
+        return this.m;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%dh%d", this.getH(), this.getM());
+    }
+
+    // =================== Other methods ====================
+    public int toMinutes() {
+        return this.h * 60 + this.m;
+    }
+
+}
