7d12b851fcfa57e22f14dea3347f39884af6b94f
lasercata <67599917+lasercata@users.noreply.github.com>
2025-05-04 15:18:50 +0200

    Correction in the bash scripts following the move of `config.properties`

--- code/get_vars.sh
+++ code/get_vars.sh
@@ -3,10 +3,9 @@
 # Script that define variables to use to connect to the DB in command line.
 # Reads from `config.properties`. See the README for details.
 
-fn="config.properties"
+fn="src/main/java/config.properties"
 db_name="tonnerre2zeus_schema"
 
-user=$(grep DB_LOGIN $fn | awk -F= '{print $2}')
-hostname=$(grep hostname $fn | awk -F/ '{print $3}' | awk -F: '{print $1}')
-password=$(grep DB_PASSWORD $fn | awk -F= '{print $2}')
-
+user=$(grep DB_LOGIN "$fn" | awk -F= '{print $2}')
+hostname=$(grep JDBC_URL "$fn" | awk -F/ '{print $3}' | awk -F: '{print $1}')
+password=$(grep DB_PASSWORD "$fn" | awk -F= '{print $2}')
